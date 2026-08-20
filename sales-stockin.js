bb66f792cd60631b8e8286623ec6f2504182cd76
works-ito <h.ito@works-rental.co.jp>
2026-08-20 19:33:48 +0900

    Advance refactor build to B14

--- sales-stockin.js
+++ sales-stockin.js
@@ -2,7 +2,7 @@
 (function() {
   "use strict";
 
-  const REFACTOR_BUILD = "B13";
+  const REFACTOR_BUILD = "B14";
 
   const title = document.querySelector(".brandTitle");
   if (title) {
@@ -10,7 +10,7 @@
   }
 
   const script = document.createElement("script");
-  script.src = "./app-bootstrap.js?v=13";
+  script.src = "./app-bootstrap.js?v=14";
   script.onerror = function() {
     console.error("リファクタ版 app-bootstrap.js の読み込みに失敗しました");
   };
