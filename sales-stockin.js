a8852f17d13c18fc66a1bc99438439162893cd01
works-ito <h.ito@works-rental.co.jp>
2026-08-20 19:05:22 +0900

    Align refactor shim with B11 build

--- sales-stockin.js
+++ sales-stockin.js
@@ -2,7 +2,7 @@
 (function() {
   "use strict";
 
-  const REFACTOR_BUILD = "B10";
+  const REFACTOR_BUILD = "B11";
 
   const title = document.querySelector(".brandTitle");
   if (title) {
@@ -10,7 +10,7 @@
   }
 
   const script = document.createElement("script");
-  script.src = "./app-bootstrap.js?v=10";
+  script.src = "./app-bootstrap.js?v=11";
   script.onerror = function() {
     console.error("リファクタ版 app-bootstrap.js の読み込みに失敗しました");
   };
